276e8412515d3633c7ee0ff2c51a8dede6b70255
Uili-Simteau <simteau@gmail.com>
2016-03-23 16:51:30 +1300

    when applied, this commit will modify add keypress event

--- javascript.js
+++ javascript.js
@@ -1,21 +1,25 @@
 var racerTable = document.querySelector('.racer_table')
 
-var Player1 = racerTable.querySelector('#player1_strip');
-var Player2 = racerTable.querySelector('#player2_strip');
-var playerPosition = racer_table.querySelector('.active');
-var tablePosition = racer_table.querySelectorAll("td");
+var player1 = document.querySelector('#player1_strip');
+var player2 = document.querySelector('#player2_strip');
+var playerPosition = document.querySelector('.active');
+var tablePosition = document.querySelectorAll("td");
 
 
 for (var i = 0; i < tablePosition.length; i++) {
-    tablePosition[i]. addEventListener("keyup", advancePosition1, false);
+    tablePosition[i]. addEventListener("keyup", updatePlayerPosition, false);
 };
 
-function advancePosition1() {
+function updatePlayerPosition(player1) {
     this.className = "active";
     this.previousElementSibling.className = "";
 }
 
-function advancePosition2(Player2) {
-    this.className = "active";
-    this.previousElementSibling.className = "";
+function advancePosition2(event) {
+    if (event.which = 13) {
+        this.className = "active";
+        this.previousElementSibling.className = "";
+    };
+
+
 }
