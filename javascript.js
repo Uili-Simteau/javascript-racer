7c5ce8eca41a65f3af6728ab316b31bf9c48aff8
Uili-Simteau <simteau@gmail.com>
2016-03-23 15:46:30 +1300

    when applied, this commit will push initial JS variable declirations

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-var updatePosition = document.querySelectorAll('td');
+var racerTable = document.getElementByClass('.racer_table')
 
+var Player1 = racer_table.querySelector('#player1_strip');
+var Player2 = racer_table.querySelector('#player2_strip');
+var playerPosition = document.querySelector('.active');
 
 
 for (var i = 0; i < updatePosition.length; i++) {
