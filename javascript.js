64b9a633b9093a3d2330da8a9ba841608e9ebe9e
Uili-Simteau <simteau@gmail.com>
2016-03-23 16:33:49 +1300

    when applied, this commit will modify racerTable variables and functions to advance player

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
-var racerTable = document.getElementByClass('.racer_table')
+var racerTable = document.querySelector('.racer_table')
 
-var Player1 = racer_table.querySelector('#player1_strip');
-var Player2 = racer_table.querySelector('#player2_strip');
+var Player1 = racerTable.querySelector('#player1_strip');
+var Player2 = racerTable.querySelector('#player2_strip');
 var playerPosition = racer_table.querySelector('.active');
 var tablePosition = racer_table.querySelectorAll("td");
 
 
 for (var i = 0; i < tablePosition.length; i++) {
-    tablePosition[i]. addEventListener("click", advancePosition);
+    tablePosition[i]. addEventListener("keyup", advancePosition1, false);
 };
 
-function advancePosition() {
+function advancePosition1() {
     this.className = "active";
+    this.previousElementSibling.className = "";
+}
+
+function advancePosition2(Player2) {
+    this.className = "active";
+    this.previousElementSibling.className = "";
 }
