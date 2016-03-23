c6388305a09cb593e71f89181055a8f735d8922a
Uili-Simteau <simteau@gmail.com>
2016-03-23 15:14:45 +1300

    with this commit, I will add my initial js code

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var updatePosition = document.querySelectorAll('td');
+
+
+
+for (var i = 0; i < updatePosition.length; i++) {
+    updatePosition[i]. addEventListener("keypress", changePosition);
+};
+
+function changePosition() {
+    if () {}
+}
