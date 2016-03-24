96738721c29496bf06c175272b01c91ab7921f6c
Uili-Simteau <simteau@gmail.com>
2016-03-25 10:10:10 +1300

    when applied, this commit will add length of track fix, and variable work inspired by lauren

--- javascript.js
+++ javascript.js
@@ -3,16 +3,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //call all the elements I think I need
   var racerTable = document.querySelector('.racer_table');
-
+  var player1 = player1Track.querySelectorAll('td');
+  var player2 = player2Track.querySelectorAll('td');
   var player1Track = document.querySelector('#player1_strip');
   var player2Track = document.querySelector('#player2_strip');
   var playerPosition = document.querySelectorAll('.active');
   var tablePosition = document.querySelectorAll("td");
-  var lengthOfTrack = "#";
+  var tracks = document.querySelectorAll("tr");
+  var start1 = 1;
+  var start2 =1;
+  var race = document.addEventListener('keyup', updatePlayerPosition);
+  var lengthOfTrack = 7;
 
 
-  var player1 = player1Track.querySelectorAll('td');
-  var player2 = player2Track.querySelectorAll('td');
+  lengthOfTrack = propmt("How far do you want to race? 1-20 parsecs?";
+
+
+//create how long the track is
+//loops for 'number' of player tracks, how many 'tr's there are -no of players?
+                for (var i = 0; i < tracks.length; i++) {
+                    //loop for 'length' of tracks
+                    for (var j = 0; j < lengthOfTrack.length; j++) {
+                        var parsec = document.createElement('td');
+                        tracks[i].appendChild(parsec);
+                    }
+                    //set last cell in array as finish line
+                    parsec.className = "planet"
+                }
 
 //starting state? where
 
